60665306a29d310a2cc26c583b8b557b987629e6
Jose <Jose@DESKTOP-8PMNA57>
2022-03-19 14:45:14 +0100

    "documentación realizada"

--- Vivienda/src/Hogar.java
+++ Vivienda/src/Hogar.java
@@ -5,8 +5,12 @@
  */
 
 /**
- *
- * @author Paqui Elena
+ * Clase Hogar
+ * 
+ * Contiene información de la vivienda que tenemos catalogada como hogar
+ * 
+ * @author Jose Maria Puertas
+ * @version 2.0
  */
 public class Hogar {
 
@@ -15,18 +19,38 @@ public class Hogar {
      */
     public static void main(String[] args) {
         Vivienda casa1 = new Vivienda(120000);
-        
+        /**
+         * precio de hogar
+         */
         double precio;
+        /**
+         * número de habitaciones del hogar
+         */
         int numHabitaciones;
+        /**
+         * superficie total del hogar en m2
+         */
         double superficie;
+        /**
+         * Si dispone o no de parking
+         */
         boolean parking;
+        /**
+         * Estado de conservación del hogar
+         */
         String estado;
+        /**
+         * Nombre y apellidos del titular de la vivienda
+         */
         String propietario;
         
         imprimirVivienda(casa1);
     }
    
-    
+    /**
+     * Este método muestra los atributos de la clase Vivienda
+     * @param casa1 Objeto de la clase Vivienda
+     */
     public static void imprimirVivienda(Vivienda casa1) {
         System.out.println("Precio: " + casa1.getPrecio());
         System.out.println("Número de habitaciones: "+ casa1.getNumHabitaciones());
--- Vivienda/src/Vivienda.java
+++ Vivienda/src/Vivienda.java
@@ -5,18 +5,48 @@
  */
 
 /**
- *
- * @author Paqui Elena
+ * Clase Vivienda
+ * 
+ * Contiene información de cada vivienda
+ * 
+ * @author Jose Maria Puertas
+ * @version 2.0
  */
 public class Vivienda 
 {
+    /**
+     * Precio de la vivienda
+     */
     private double precio;
+    /**
+    * Número de habitaciones de la vivienda
+    */
     private int numHabitaciones;
+    /**
+     * Superficie total de la vivienda en m2
+     */
     private double superficie;
+    /**
+     * Dispone o no de parking la vivienda
+     */
     private boolean parking;
+    /**
+     * Estado de conservación en el que se encuentra la vivienda: Bueno, Normal, Regular, Malo o Ruinoso
+     */
     private String estado;
+    /**
+     * Nombre y apellidos del propietario de la vivienda
+     */
     private String propietario;
-
+    /**
+    * Constructor de la clase Vivienda donde se le introducen todos los parametros
+    * @param precio precio de la vivienda en euros
+    * @param numHabitaciones número de habitaciones incluidos baños, cocinas, comedor, etc.
+    * @param superficie superficie total de la vivienda en metros cuadrados
+    * @param parking si dispone de parking o no
+    * @param estado estado de conservación el que se encuentra la vivienda
+    * @param propietario Nombre y apellidos del titular de la vivienda
+    */
     public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado, String propietario) {
         this.precio = precio;
         this.numHabitaciones = numHabitaciones;
@@ -26,13 +56,18 @@ public class Vivienda
         this.propietario = propietario;
     }
 
-    
-    
+    /**
+     * Construtor de la clase Vivienda donde solo se le introduce el nuevo valor del precio
+     * @param precio precio de la vivienda en euros
+     */    
     public Vivienda (double precio)
     {
         this.precio = precio;
     }
-    
+    /**
+     * Este metodo aplica un descuento sobre el precio original
+     * @param descuento porcentaje en tanto por uno a restar del precio actual
+     */
     public void actualizarPrecio(double descuento)
     {
         setPrecio(getPrecio() - getPrecio() * descuento);
